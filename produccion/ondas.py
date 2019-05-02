11af45968b4787ef2ac110fdc957d5072476f8b8
John Michael <jmichaelten@gmail.com>
2019-05-01 22:14:19 -0500

    Onda cuadrada en display, con parámetros de amplitud y frecuencia variables

--- produccion/ondas.py
+++ produccion/ondas.py
@@ -4,6 +4,8 @@
 import math
 import time
 import calculadora
+from machine import Pin, SPI
+import pcd8544
 import gc
 #ecuacion de fourier para generar un pulso rectangular con la ecuacion del fenomeno de gibbs    
 '''
@@ -15,8 +17,26 @@ http://mathworld.wolfram.com/FourierSeriesSquareWave.html
 
 '''
 
-def urectangular(max, puntos, K, amplitud):
+def urectangular(max, puntos, K, amplitud, w0):
     print("urectangular")
+
+    import framebuf
+    spi = SPI(1, baudrate=328125, polarity=0, phase=0)
+    cs = Pin(2) #D4
+    dc = Pin(15) #D8
+    rst = Pin(0) #D3
+    #bl = Pin(12, Pin.OUT, value=1) #D6
+    print("Variables:OK")
+    lcd = pcd8544.PCD8544(spi, cs, dc, rst)
+    print("lcd ok")
+    buffer = bytearray((lcd.height // 8) * lcd.width)
+    framebuf = framebuf.FrameBuffer1(buffer, lcd.width, lcd.height)
+    framebuf.fill(1)
+    lcd.data(buffer)
+    time.sleep(1)
+    framebuf.fill(0)
+    lcd.data(buffer)
+
     print("Iniciando...")
     count=0
     y_record=[] #
@@ -28,9 +48,9 @@ def urectangular(max, puntos, K, amplitud):
         #print("k -> ",k)
         for t in T:
             #print("Operacion: ", "k -> ",k, ", t -> ",t)
-            y = amplitud*(4/math.pi)*(math.sin((2*k-1)*t)/(2*k-1))
+            y = amplitud*(4/math.pi)*(math.sin(w0*(2*k-1)*t)/(2*k-1))
             y_record.append(y) #Lista de puntos de la funcion.
-            time.sleep_ms(40)
+            time.sleep_ms(10)
         count=count+1
         gc.collect()
         #diseno: esta agregando a la lista ambas funciones.
@@ -43,7 +63,7 @@ def urectangular(max, puntos, K, amplitud):
                 #print("Suma: ", i, " con ", i+(K-1)*puntos )
                 #time.sleep(0.2)
                 y_res.append(y_record[i]+y_record[i+(K-1)*puntos])
-                time.sleep_ms(40)
+                time.sleep_ms(10)
     K=K-1
     
     while K!=1:
@@ -51,12 +71,23 @@ def urectangular(max, puntos, K, amplitud):
                 #print("Suma while: ", i, " con ", i+(K-1)*puntos )
                 #time.sleep(0.2)
                 y_res[i]=y_res[i]+y_record[i+(K-1)*puntos]
-                time.sleep_ms(40)
+                time.sleep_ms(10)
         print("Tamano del arreglo res", len(y_res), "presicion: ",K)
         K=K-1
     print(len(y_res))
 
-
+    count=1
+    for y in y_res:
+        print(y,round(y))
+        time.sleep_ms(10)
+        framebuf.pixel(count,24-round(y),1)
+        count=count+1
+    #eje y
+    framebuf.vline(0, 0, 96, 0xffff)
+    #eje x
+    framebuf.hline(0, 24, 96, 0xffff)
+    #escribiendo datas
+    lcd.data(buffer)
     
     
 
@@ -68,12 +99,18 @@ if __name__ == "__main__":
     #input: max, puntos para ulinspace. Presicion = n, {n E N}. Indica que tan parecida debe ser la funcion a la curva ideal
     max=84
     puntos=84 #no funciona para puntos>200
-    presicion=10 #K =presicion
-    amplitud=1
-    urectangular(max, puntos, presicion, amplitud)
+    presicion=8 #K =presicion
+    amplitud=14
+    w0=0.09
+    phi=0
+    urectangular(max, puntos, presicion, amplitud, w0)
 
 else:
     print("importado gibss")
+
+
+
+
 
 
 
