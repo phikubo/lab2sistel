50fe1956d4e5a299f7840137634e0443f03a1b23
John Michael <jmichaelten@gmail.com>
2019-05-31 10:48:00 -0500

    Testing Concurrency on hold

--- testing/threadfunctions.py
+++ testing/threadfunctions.py
@@ -7,36 +7,34 @@ import math
 import gc
 #los pines estan ajustados a GPIO y no a las etiquetas internas.
 #test reflush directory, not working
-def test1():
+def test1(myloc):
+    myloc = _thread.allocate_lock()
+    myloc.acquire()
     for i in range(500):
-        time.sleep(1)
-        print(i)
+        time.sleep(0.2)
+        print(1, "{",i,"}")
     print("end test1")
+    return myloc
     
-def test2():
+def test2(myloc):
+    myloc.release()
     for i in range(500):
-        time.sleep(1)
-        print(i)
+        time.sleep(0.2)
+        print(2, "{",i,"}")
     print("end test2")
 
 
 if __name__ == "__main__":
     print("Esp Funcionando correctamente")
-    #test()
+    myloc = _thread.allocate_lock()
     gc.collect()
-    print("Test thread1")
+    print("Prueba de bloqueo")
     time.sleep(1)
-    test = _thread.allocate_lock()
-    test.acquire(1)
-    test.start_new_thread(tes1, ())
-    
-    print("Test thread2")
+    _thread.start_new_thread(test1, (myloc,))
+    _thread.start_new_thread(test2,(myloc,))
+    print("Fin prueba")
     time.sleep(1)
-    #gc.collect()
-    #test1.release()
-    #_thread.start_new_thread(test2, ())
-    #test.release()
-    
+    gc.collect()
 else:
     pass
 
